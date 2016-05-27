0014c651b08cfe9f2ee62363ce6ca3465e32f171
Jeffrey Horn <jeffh@generalassemb.ly>
2016-05-27 12:42:04 -0400

    Change application name (+ important note!)
    From `ga-wdi-boston.ember-template` to `ember-overview`.
    
    We can't call the app `ember`, which would be consistent with our naming
    conventions, since Ember itself is named `ember`. 😄
    
    Close #8

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-template',
+    modulePrefix: 'ember-overview',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
